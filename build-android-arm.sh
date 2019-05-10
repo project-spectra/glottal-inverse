aaf5b6dd44571926f9004a7969ccbe9bf2fef1a0
Chloé Ichinose <chloedv99@gmail.com>
2019-05-11 00:54:24 +0200

    Added distinction between ARM and ARM64 in binary file name

--- build-android-arm.sh
+++ build-android-arm.sh
@@ -8,7 +8,7 @@ cd build-android-arm
 cmake -DCMAKE_C_COMPILER=$TOOLCHAIN/bin/arm-linux-androideabi-clang \
       -DCMAKE_CXX_COMPILER=$TOOLCHAIN/bin/arm-linux-androideabi-clang++ \
       -DCMAKE_COLOR_MAKEFILE=1 \
-      -DTARGET_ARCH=Android \
+      -DTARGET_ARCH=Android_ARM \
       -DCMAKE_BUILD_TYPE=$1 \
       ..
 
--- build-android-arm64.sh
+++ build-android-arm64.sh
@@ -8,7 +8,7 @@ cd build-android-arm64
 cmake -DCMAKE_C_COMPILER=$TOOLCHAIN/bin/aarch64-linux-android-clang \
       -DCMAKE_CXX_COMPILER=$TOOLCHAIN/bin/aarch64-linux-android-clang++ \
       -DCMAKE_COLOR_MAKEFILE=1 \
-      -DTARGET_ARCH=Android \
+      -DTARGET_ARCH=Android_ARM64 \
       -DCMAKE_BUILD_TYPE=$1 \
       ..
 
